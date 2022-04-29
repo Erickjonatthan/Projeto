580d7cd7c7fd7400b075705cf38e10ccf83189f8
Erick Jonathan <65685466+Erickjonatthan@users.noreply.github.com>
2022-04-28 21:06:27 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,6 +1,11 @@
+<?php
 $servidor = "localhost";
 $usuario = "root";
 $senha = "";
 
-$banco = myslq_connect($servidor, $usuario, $senha) or
-die("Não foi pssivel conectar ao servidor do BD")
+$conexao = mysqli_connect($servidor, $usuario, $senha) or
+die("Não foi pssivel conectar ao servidor do BD");
+
+mysqli_select_db($tabela, $conexao) or
+die("Não foi pssivel conectar a tabela do BD");
+?>
